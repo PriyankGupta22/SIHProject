b61bdccdad13ff1e79c3819fe4237e2a28088073
dictator110 <blackbot320@gmail.com>
2025-09-05 17:36:55 +0530

    Resolved merge conflicts after stash pop

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Login from './Login/Login'
 import './App.css'
+import App1 from  "./app1"
+import "./index.css"
 
 function App() {
   
 
   return (
     <>
+<<<<<<< Updated upstream
     <Login/>
+=======
+    <App1></App1>
+    {/* <div className='container'>
+     I am a new box 
+     Priyanshu Bhadwe :)
+    </div> */}
+      
+>>>>>>> Stashed changes
     </>
   )
 }
