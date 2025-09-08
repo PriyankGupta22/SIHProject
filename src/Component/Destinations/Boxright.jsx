0b84a43d6963365706c17492b5fff4ab760a1845
PriyankGupta22 <gsanu8092@gmail.com>
2025-09-08 22:16:39 +0530

    added some buttons

--- src/Component/Destinations/Boxright.jsx
+++ src/Component/Destinations/Boxright.jsx
@@ -4,6 +4,7 @@ import './Boxright.css'
 import star from '../../assets/star.svg'
 import locbox from '../../assets/locbox.svg'
 import clock from '../../assets/clock.svg'
+import eye from '../../assets/eye.svg'
 
 
 function Boxright({
@@ -24,6 +25,19 @@ function Boxright({
         <div className="boxInfocont">
           {titlediscription}
         </div>
+        <div className="viewButtons" style={{marginTop:10}}>
+                  <button className="arview" style={{height:35,width:130,backgroundColor:'white',color:'black',borderRadius:5,border:'2px solid black',display:'flex',justifyContent:'center',alignItems:'center',gap:5}}>
+                    <div className="eye">
+                      <img style={{height:18,width:18,marginTop:2}} src={eye}/>
+                    </div>
+                    <div className="arviewInfo" style={{fontSize:13}}>
+                      AR/VR Preview
+                    </div>
+                  </button>
+                  <button className="planVisit" style={{height:35,width:130,backgroundColor:'black',color:'white',borderRadius:5,border:0,fontSize:15}}>
+                    Plan Visit
+                  </button>
+                </div>
     </div>
 
     <div className="boxDivright" style={{backgroundImage:`url(${backImg})` , backgroundSize: 'cover', backgroundPosition: 'center' }}>
